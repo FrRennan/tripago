de7da7db495375364e7c41013c7191bb4f28e406
Rennan Oliveira <franciscorennan.eeepeq@gmail.com>
2023-05-18 14:54:35 -0300

    first commit

---
+++ src/components/TripList.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import { useFetch } from '../hooks/useFetch'
+
+// styles
+import './TripList.css'
+
+export default function TripList() {
+    //const [trips, setTrips] = useState([])
+    const [url, setUrl] = useState('http://localhost:3000/trips')
+    const { data: trips, isPending, error } = useFetch(url, { type: 'GET' })
+
+    return (
+        <div className="trip-list">
+            <h2>Trip List</h2>
+            {isPending && <div>Loading trips...</div>}
+            {error && <div>{error}</div>}
+            <ul>
+                {trips && trips.map(trip => (
+                    <li key={trip.id}>
+                        <h3>{trip.title}</h3>
+                        <p>{trip.price}</p>
+                    </li>
+                ))}
+            </ul>
+            <div className="filters">
+                {/* filtra a url para obters as viagens com loc europe */}
+                <button onClick={() => setUrl('http://localhost:3000/trips?loc=europe')}>
+                    European Trips
+                </button>
+                <button onClick={() => setUrl('http://localhost:3000/trips')}>
+                    All Trips
+                </button>
+            </div>
+        </div>
+    )
+}
